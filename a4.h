e37336da3039981615b90b4399566f1cc63f8fb2
Camille Cua <ccua@uoguelph.ca>
2019-11-06 20:11:09 -0500

    added .c and .h file

---
+++ a4.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int hash1(char *string, int hash_size);
+int hash2(char *string, int hash_size);
+int hash3(char *string, int hash_size);
