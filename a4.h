24606c276dd47ea2ae8281b5e8d227e7cf3512d5
Camille Cua <ccua@uoguelph.ca>
2019-11-15 10:47:17 -0500

    adding files to git

--- a4.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-int hash1(char *string, int hash_size);
-int hash2(char *string, int hash_size);
-int hash3(char *string, int hash_size);
